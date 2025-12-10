2cf34750818eb55a8c5908bd8f5f5f39a7f5d729
Ashutosh Kumar <ashutoshk043@gmail.com>
2025-12-10 21:55:28 +0530

    updated codes

--- apps/foodservers/orderservice/src/orderresolver/orderresolver.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class OrderresolverResolver {
-      @Query(() => String)
-      hello1() {
-        return 'Hello from Order Resolver!';
-      }
-}
--- apps/foodservers/productservice/src/product/product.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class ProductResolver {
-     @Query(() => String)
-          hello2() {
-            return 'Hello from Product Resolver!';
-          }
-}
